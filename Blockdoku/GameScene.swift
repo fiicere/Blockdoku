bdfb836554e0f00b757d5aa93d2766eea4cf6a7a
fiicere <fiicere@gmail.com>
2015-04-06 17:32:21 -0400

    IntroScene
    transitions to gameScene on click

--- Blockdoku/GameScene.swift
+++ Blockdoku/GameScene.swift
@@ -6,17 +6,12 @@
 //  Copyright (c) 2015 Kevin Yue. All rights reserved.
 //
 
+import Foundation
 import SpriteKit
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+  
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
@@ -38,7 +33,7 @@ class GameScene: SKScene {
             self.addChild(sprite)
         }
     }
-   
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
---
+++ Blockdoku/IntroScene.swift
@@ -0,0 +1,30 @@
+//
+//  GameScene.swift
+//  Blockdoku
+//
+//  Created by Kevin Yue on 4/6/15.
+//  Copyright (c) 2015 Kevin Yue. All rights reserved.
+//
+
+import SpriteKit
+
+class IntroScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+        myLabel.text = "Hello, World!";
+        myLabel.fontSize = 65;
+        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        
+        self.addChild(myLabel)
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        var scene:SKScene = GameScene(size: self.size)
+        self.view?.presentScene(scene, transition:SKTransition.fadeWithDuration(1))
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
